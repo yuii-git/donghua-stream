1e1348499c2876979f31b869722be50f89612bd2
yuii-git <alkhabsy270@gmail.com>
2026-06-29 18:17:57 +0700

    Add files via upload

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,1 +1,35 @@
+import axios from 'axios';
 
+const api = axios.create({
+  baseURL: '/api'
+});
+
+export const getDonghuaOngoing = async (page = 1) => {
+  const res = await api.get(`/donghua/ongoing?page=${page}`);
+  return res.data;
+};
+
+export const getAnimeOngoing = async (page = 1) => {
+  const res = await api.get(`/anime/ongoing?page=${page}`);
+  return res.data;
+};
+
+export const getDonghuaCompleted = async (page = 1) => {
+  const res = await api.get(`/donghua/completed?page=${page}`);
+  return res.data;
+};
+
+export const searchAnime = async (query: string, page = 1, mode = 'donghua') => {
+  const res = await api.get(`/search`, { params: { q: query, page, mode } });
+  return res.data;
+};
+
+export const getDetail = async (slug: string, mode = 'donghua') => {
+  const res = await api.get(`/detail/${slug}`, { params: { mode } });
+  return res.data;
+};
+
+export const getEpisode = async (slug: string, mode = 'donghua') => {
+  const res = await api.get(`/episode/${slug}`, { params: { mode } });
+  return res.data;
+};
